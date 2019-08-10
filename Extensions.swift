d00fb086c7d379e1c6868d0d14a74bb6a9cdecc9
Wilfred Ruck <wilfredoruck@gmail.com>
2019-08-10 14:11:27 -0400

    complete code

--- Extensions.swift
+++ Extensions.swift
@@ -10,4 +10,38 @@ import Foundation
 import SceneKit
 import ARKit
 
+var width : CGFloat = 0.02
+var height : CGFloat = 1
+var length : CGFloat = 1
 
+var doorLength : CGFloat = 0.3
+
+func createBox(isDoor : Bool) -> SCNNode {
+    let node = SCNNode()
+    
+    // Add First Box
+    let firstBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    let firstBoxNode = SCNNode(geometry: firstBox)
+    firstBoxNode.renderingOrder = 200
+    
+    node.addChildNode(firstBoxNode)
+    
+    // Add Masked Box
+    let maskedBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    maskedBox.firstMaterial?.diffuse.contents = UIColor.white
+    maskedBox.firstMaterial?.transparency = 0.00001
+    
+    let maskedBoxNode = SCNNode(geometry: maskedBox)
+    maskedBoxNode.renderingOrder = 100
+    maskedBoxNode.position = SCNVector3.init(width, 0, 0)
+    
+    node.addChildNode(maskedBoxNode)
+    
+    return node
+}
+
+extension FloatingPoint {
+    var degreesToRadians : Self {
+        return self * .pi / 180
+    }
+}
